f311798a4f4e418e14259d30141d0eb45356ff6e
GabKenKL <gabkenkudodelima@gmail.com>
2026-05-05 15:22:30 -0300

    Primeira versão dos códigos

---
+++ Ex.31 Quadrado dos número entre 10 e 150.py
@@ -0,0 +1,5 @@
+#algorítmo repetição
+#Calcule e mostre o quadrado dos números entre 10 e 150.
+for numero in range(11,150):
+    quadrado = numero ** 2
+    print(f"O quadrado de {numero} é {quadrado}")
